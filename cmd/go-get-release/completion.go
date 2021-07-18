779067d064f6b132f0753d8ccfeb2adda61209ca
Takao Shibata <chise.alter.pasta@gmail.com>
2021-07-18 15:46:09 +0900

    Implement bash-completion (#47)

---
+++ cmd/go-get-release/completion.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+var bashCompletion = `
+__ggr_bash_autocomplete() {
+  if [[ "${COMP_WORDS[0]}" != "source" ]]; then
+    local cur opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    if [[ "$cur" == "-"* ]]; then
+      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} ${cur} --generate-bash-completion )
+    else
+      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+    fi
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  fi
+}
+
+complete -o bashdefault -o default -o nospace -F __ggr_bash_autocomplete go-get-release
+`
+
+func completion(shell string) error {
+	if shell != "bash" {
+		return fmt.Errorf("supported shell is bash only")
+	}
+	fmt.Println(bashCompletion)
+	return nil
+}
