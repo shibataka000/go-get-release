b691ad332bd09039a88bacd8be221b9372b1df4a
Takao Shibata <chise.alter.pasta@gmail.com>
2023-01-14 15:35:50 +0900

    test: add test case

--- internal/infrastructure/embed/execbinary_test.go
+++ internal/infrastructure/embed/execbinary_test.go
@@ -4,25 +4,22 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/shibataka000/go-get-release/internal/domain/model/asset"
+	"github.com/shibataka000/go-get-release/internal/domain/model/execbinary"
 	"github.com/shibataka000/go-get-release/internal/domain/model/platform"
-	"github.com/shibataka000/go-get-release/internal/domain/model/release"
 	"github.com/shibataka000/go-get-release/internal/domain/model/repository"
 	"github.com/stretchr/testify/require"
 )
 
-func TestFindAssetMetadata(t *testing.T) {
+func TestFindExecBinaryMetadata(t *testing.T) {
 	tests := []struct {
 		repo     repository.Repository
-		release  release.Release
 		platform platform.Platform
-		meta     asset.Metadata
+		meta     execbinary.Metadata
 	}{
 		{
-			repo:     repository.New("hashicorp", "terraform"),
-			release:  release.New(0, "v1.3.7"),
+			repo:     repository.New("cli", "cli"),
 			platform: platform.New("linux", "amd64"),
-			meta:     asset.NewMetadata("https://releases.hashicorp.com/terraform/1.3.7/terraform_1.3.7_linux_amd64.zip"),
+			meta:     execbinary.NewMetadata("gh", platform.New("linux", "amd64")),
 		},
 	}
 
@@ -32,31 +29,28 @@ func TestFindAssetMetadata(t *testing.T) {
 			assert := require.New(t)
 			repository, err := New()
 			assert.NoError(err)
-			meta, err := repository.FindAssetMetadata(tt.repo, tt.release, tt.platform)
+			meta, err := repository.FindExecBinaryMetadata(tt.repo, tt.platform)
 			assert.NoError(err)
 			assert.Equal(tt.meta, meta)
 		})
 	}
 }
 
-func TestHasAssetMetadata(t *testing.T) {
+func TestHasExecBinaryMetadata(t *testing.T) {
 	tests := []struct {
 		repo        repository.Repository
-		release     release.Release
 		platform    platform.Platform
 		hasMetadata bool
 	}{
 		{
 			repo:        repository.New("hashicorp", "terraform"),
-			release:     release.New(0, "v1.3.7"),
-			platform:    platform.New("linux", "amd64"),
-			hasMetadata: true,
-		},
-		{
-			repo:        repository.New("argoproj", "argo-rollouts"),
-			release:     release.New(0, "v0.9.0"),
 			platform:    platform.New("linux", "amd64"),
 			hasMetadata: false,
+		},
+		{
+			repo:        repository.New("cli", "cli"),
+			platform:    platform.New("linux", "amd64"),
+			hasMetadata: true,
 		},
 	}
 
@@ -66,7 +60,7 @@ func TestHasAssetMetadata(t *testing.T) {
 			assert := require.New(t)
 			repository, err := New()
 			assert.NoError(err)
-			hasAssetMetadata := repository.HasAssetMetadata(tt.repo, tt.release, tt.platform)
+			hasAssetMetadata := repository.HasExecBinaryMetadata(tt.repo, tt.platform)
 			assert.Equal(tt.hasMetadata, hasAssetMetadata)
 		})
 	}
