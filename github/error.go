b21085378ace72b12ba70f4919731cdc3ffbfa7d
Takao Shibata <chise.alter.pasta@gmail.com>
2024-06-09 23:37:38 +0900

    save

--- github/error.go
+++ github/error.go
@@ -11,16 +11,9 @@ func (e *InvalidRepositoryFullNameFormatError) Error() string {
 	return fmt.Sprintf("'%s' is not valid repository full name. Its format must be 'OWNER/NAME'.", e.fullName)
 }
 
-// AssetNotFoundError is error raised when no AssetMeta was found.
+// AssetNotFoundError is error raised when GitHub release asset was not found.
 type AssetNotFoundError struct{}
 
 func (e *AssetNotFoundError) Error() string {
 	return "No asset was found."
-}
-
-// AssetNotFoundError is error raised when no ExecutableBinaryMeta was found.
-type ExecutableBinaryNotFoundError struct{}
-
-func (e *ExecutableBinaryNotFoundError) Error() string {
-	return "No executable binary was found."
 }
