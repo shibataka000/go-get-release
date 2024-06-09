491bdc7005815cc88a237ca20407b3a888601819
Takao Shibata <chise.alter.pasta@gmail.com>
2024-06-09 23:31:12 +0900

    save

--- github/helper.go
+++ github/helper.go
@@ -2,6 +2,7 @@ package github
 
 import "text/template"
 
+// newTemplate allocates a new template with given name and parses text as a template body for it.
 func newTemplate(name string, text string) *template.Template {
 	return template.Must(template.New(name).Parse(text))
 }
