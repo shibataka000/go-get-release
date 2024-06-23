1455fd573a56efdee76fbffab6232949e281d311
Takao Shibata <chise.alter.pasta@gmail.com>
2024-06-23 14:34:45 +0900

    update

--- github/asset_test.go
+++ github/asset_test.go
@@ -155,7 +155,7 @@ func TestListAssets(t *testing.T) {
 		assets  AssetList
 	}{
 		{
-			name:    "terraform_1.8.5_linux_amd64.zip",
+			name:    "cli/cli",
 			repo:    newRepository("cli", "cli"),
 			release: newRelease("v2.21.1"),
 			assets: AssetList{
@@ -179,6 +179,16 @@ func TestListAssets(t *testing.T) {
 				newAsset(newURL("https://github.com/cli/cli/releases/download/v2.21.1/gh_2.21.1_windows_arm64.zip"), mime.Zip),
 			},
 		},
+		{
+			name:    "hashicorp/terraform",
+			repo:    newRepository("hashicorp", "terraform"),
+			release: newRelease("v1.8.0"),
+			assets: AssetList{
+				newAsset(newURL("https://releases.hashicorp.com/terraform/1.8.0/terraform_1.8.0_linux_amd64.zip"), mime.Zip),
+				newAsset(newURL("https://releases.hashicorp.com/terraform/1.8.0/terraform_1.8.0_darwin_amd64.zip"), mime.Zip),
+				newAsset(newURL("https://releases.hashicorp.com/terraform/1.8.0/terraform_1.8.0_windows_amd64.zip"), mime.Zip),
+			},
+		},
 	}
 
 	for _, tt := range tests {
@@ -192,66 +202,3 @@ func TestListAssets(t *testing.T) {
 		})
 	}
 }
-
-// func TestListAssets(t *testing.T) {
-
-// 	tests := []struct {
-// 		name    string
-// 		repo    Repository
-// 		release Release
-// 		assets  AssetList
-// 	}{
-// 		{
-// 			name:    "shibataka000/go-get-release-test",
-// 			repo:    newRepository("shibataka000", "go-get-release-test"),
-// 			release: newRelease("v0.0.2"),
-// 			assets: AssetList{
-// 				newAsset("https://github.com/shibataka000/go-get-release-test/releases/download/v0.0.2/go-get-release_v0.0.2_darwin_amd64", ""),
-// 				newAsset("https://github.com/shibataka000/go-get-release-test/releases/download/v0.0.2/go-get-release_v0.0.2_linux_amd64", ""),
-// 				newAsset("https://github.com/shibataka000/go-get-release-test/releases/download/v0.0.2/go-get-release_v0.0.2_windows_amd64.exe", ""),
-// 			},
-// 		},
-// 	}
-
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			require := require.New(t)
-// 			ctx := context.Background()
-// 			repository := NewAssetRepository(ctx, os.Getenv("GITHUB_TOKEN"))
-// 			assets, err := repository.list(ctx, tt.repo, tt.release)
-// 			require.NoError(err)
-// 			require.Equal(tt.assets, assets)
-// 		})
-// 	}
-// }
-
-// func TestListAssetsFromBuiltIn(t *testing.T) {
-// 	tests := []struct {
-// 		name    string
-// 		repo    Repository
-// 		release Release
-// 		assets  AssetList
-// 	}{
-// 		{
-// 			name:    "hashicorp/terraform",
-// 			repo:    newRepository("hashicorp", "terraform"),
-// 			release: newRelease("v1.0.0"),
-// 			assets: AssetList{
-// 				newAsset("https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_linux_amd64.zip", ""),
-// 				newAsset("https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_darwin_amd64.zip", ""),
-// 				newAsset("https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_windows_amd64.zip", ""),
-// 			},
-// 		},
-// 	}
-
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			require := require.New(t)
-// 			ctx := context.Background()
-// 			repository := NewAssetRepository(ctx, os.Getenv("GITHUB_TOKEN"))
-// 			assets, err := repository.listExternal(tt.repo, tt.release)
-// 			require.NoError(err)
-// 			require.Equal(tt.assets, assets)
-// 		})
-// 	}
-// }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/shibataka000/go-get-release
 
-go 1.20
+go 1.22
 
 require (
 	github.com/Songmu/prompter v0.5.1
