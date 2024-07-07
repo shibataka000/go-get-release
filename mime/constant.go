fb8809805bad41d4d91b9baa76a8d0b025ed3767
Takao Shibata <chise.alter.pasta@gmail.com>
2024-07-07 21:41:49 +0900

    save

---
+++ mime/constant.go
@@ -0,0 +1,16 @@
+package mime
+
+const (
+	// CompressedTar represents MIME for compressed tar file.
+	CompressedTar = "application/x-compressed-tar"
+	// Gzip represents MIME for gzip file.
+	Gzip = "application/gzip"
+	// OctedStream represents MIME for binary file.
+	OctedStream = "application/octet-stream"
+	// Tar represents MIME for tar file.
+	Tar = "application/x-tar"
+	// Xz represents MIME for xz file.
+	Xz = "application/x-xz"
+	// Zip represents MIME for zip file.
+	Zip = "application/zip"
+)
