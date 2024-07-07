b66e24b6dd60c0022c9c823bfd95c1e18c845686
Takao Shibata <chise.alter.pasta@gmail.com>
2024-07-07 21:11:56 +0900

    helper

--- platform/platform.go
+++ platform/platform.go
@@ -18,21 +18,3 @@ func Detect(name string) (OS, Arch) {
 	arch := findKeyWhichHasLongestMatchValue(archKeywords, name)
 	return os, arch
 }
-
-// findKeyWhichHasLongestMatchValue returns key in map which has longest matched value with target.
-// If no value was matched with target, this returns defaultKey.
-func findKeyWhichHasLongestMatchValue[E ~string](m map[E][]string, target string) E {
-	var (
-		matchKey   E
-		matchValue string
-	)
-	for key, values := range m {
-		for _, value := range values {
-			if strings.Contains(target, value) && len(matchValue) < len(value) {
-				matchKey = key
-				matchValue = value
-			}
-		}
-	}
-	return matchKey
-}
