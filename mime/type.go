7e626b0479e0aa0e636c3606eec7527939a196af
Takao Shibata <chise.alter.pasta@gmail.com>
2024-07-08 23:38:13 +0900

    save

--- mime/type.go
+++ mime/type.go
@@ -1,5 +1,20 @@
 package mime
 
+const (
+	// CompressedTar represents MIME for compressed tar file.
+	CompressedTar = "application/x-compressed-tar"
+	// Gzip represents MIME for gzip file.
+	Gzip = "application/gzip"
+	// OctedStream represents MIME for binary file.
+	OctedStream = "application/octet-stream"
+	// Tar represents MIME for tar file.
+	Tar = "application/x-tar"
+	// Xz represents MIME for xz file.
+	Xz = "application/x-xz"
+	// Zip represents MIME for zip file.
+	Zip = "application/zip"
+)
+
 // compressed is a list of MIME type of compressed file.
 // See https://en.wikipedia.org/wiki/List_of_archive_formats more details.
 var compressed = []MIME{
