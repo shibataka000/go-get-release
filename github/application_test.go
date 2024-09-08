b50320a998f33643213af94b1ff8011b61777e5b
Takao Shibata <chise.alter.pasta@gmail.com>
2024-09-08 16:22:24 +0900

    save

--- github/application_test.go
+++ github/application_test.go
@@ -2,26 +2,19 @@ package github
 
 import (
 	"context"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestApplicationService(t *testing.T) {
+func TestApplicationServiceFindAsset(t *testing.T) {
 	tests := []struct {
-		name string
-
-		repoFullName       string
-		tag                string
-		assetPatterns      []string
-		execBinaryPatterns []string
-
-		asset      Asset
-		execBinary ExecBinary
-	}{
-		{},
-	}
+		name          string
+		repoFullName  string
+		tag           string
+		assetPatterns []string
+		asset         Asset
+	}{}
 
 	ctx := context.Background()
 	app := NewApplicationService(
@@ -32,16 +25,59 @@ func TestApplicationService(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			require := require.New(t)
-
 			asset, err := app.FindAsset(ctx, tt.repoFullName, tt.tag, tt.assetPatterns)
 			require.NoError(err)
 			require.Equal(tt.asset, asset)
+		})
+	}
+}
 
-			execBinary, err := app.FindExecBinary(asset, tt.assetPatterns, tt.execBinaryPatterns)
+func TestApplicationServiceFindExecBinary(t *testing.T) {
+	tests := []struct {
+		name               string
+		asset              Asset
+		assetPatterns      []string
+		execBinaryPatterns []string
+		execBinary         ExecBinary
+	}{}
+
+	ctx := context.Background()
+	app := NewApplicationService(
+		NewAssetRepository(ctx, githubToken),
+		NewExecBinaryRepository(),
+	)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+			execBinary, err := app.FindExecBinary(tt.asset, tt.assetPatterns, tt.execBinaryPatterns)
 			require.NoError(err)
 			require.Equal(tt.execBinary, execBinary)
+		})
+	}
+}
 
-			file, err := os.CreateTemp("", "")
+func TestApplicationServiceInstall(t *testing.T) {
+	tests := []struct {
+		name               string
+		asset              Asset
+		assetPatterns      []string
+		execBinaryPatterns []string
+		execBinary         ExecBinary
+	}{}
+
+	ctx := context.Background()
+	app := NewApplicationService(
+		NewAssetRepository(ctx, githubToken),
+		NewExecBinaryRepository(),
+	)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+			execBinary, err := app.FindExecBinary(tt.asset, tt.assetPatterns, tt.execBinaryPatterns)
+			require.NoError(err)
+			require.Equal(tt.execBinary, execBinary)
 		})
 	}
 }
