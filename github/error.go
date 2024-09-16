bac4e945b243f58fd89fb7ba7d2ad6999d1fb2b2
Takao Shibata <chise.alter.pasta@gmail.com>
2024-09-16 09:55:05 +0900

    err

--- github/error.go
+++ github/error.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	ErrInvalidRepositoryFullName          = errors.New("repository full name was not 'OWNER/REPO' format")
-	ErrUnpairablePattern                  = errors.New("the number of asset pattern and the number of exec binary pattern were not same")
+	ErrUnpairablePattern                  = errors.New("the number of asset patterns and the number of exec binary patterns were not same")
 	ErrPatternNotMatched                  = errors.New("no pattern matched with any release asset name")
 	ErrUnexpectedMIME                     = errors.New("unexpected mime type")
 	ErrExtractingExecBinaryContentFailure = errors.New("extracting exec binary content from release asset content was failed")
